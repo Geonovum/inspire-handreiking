d4db959c1429dfb9941e9d9210e94366f04a7658
Wideke Boersma <76045152+wboersma@users.noreply.github.com>
2022-02-01 12:13:02 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,18 +2,18 @@ var respecConfig = {
   specStatus: "GN-WV", // Kies uit de lijst: https://github.com/Geonovum/respec/wiki/specStatus
   specType: "HR", // Kies uit de lijst: https://github.com/Geonovum/respec/wiki/specType
   //pubDomain: "wp", // Kies uit de lijst: https://github.com/Geonovum/respec/wiki/pubDomain. Of vraag aan bij beheerders
-  publishDate: "2021-10-14", // Datum als jjjj-mm-dd, bijvoorbeeld
+  publishDate: "2022-02-01", // Datum als jjjj-mm-dd, bijvoorbeeld
   editors: [
     {
-      name: "Geonovum",
+      name: "Geonovum INSPIRE team",
       company: "Geonovum",
       companyURL: "http://www.geonovum.nl/",
     }
     ],
-  shortName: "inspire-wiki",
+  shortName: "inspire-handreiking",
   subtitle: "Voldoen aan de INSPIRE-verplichtingen",
-  github: "https://github.com/Geonovum/inspire-wiki",
-  issueBase: "https://github.com/Geonovum/inspire-wiki/issues/",
+  github: "https://github.com/Geonovum/inspire-handreiking",
+  issueBase: "https://github.com/Geonovum/inspire-handreiking/issues/",
   license: 'cc-by-nd',
   // logos: [], // Geef een lege array op als er geen Geonovum logo moet staan
   doJsonLd: true,
